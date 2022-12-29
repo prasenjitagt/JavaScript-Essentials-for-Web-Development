939dc8b8c071167b7b10f7e09e95dc79433200f3
Prasenjit Das <businesspaymentsagt@gmail.com>
2022-12-29 07:55:49 +0530

    Array Destructuring

---
+++ 02_Array_Destructuring/destucturing.js
@@ -0,0 +1,40 @@
+
+
+const animals = ['tiger','bear','dog','cat','cow'];
+
+const persons = ['rakesh','viskash','prity','sonya'];
+
+
+// const animal1 = animals[0];
+// const animal2 = animals[1];
+// const animal3 = animals[2];
+
+const [animal1,,,,,,animal6] = ['tiger','bear','dog','cat','cow','lion'];
+
+
+console.log(animal1,animal6);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Destructuring
+// Destructuring is a faster way to access the elements of an array or object;
+// In this video we will cover Array Destructuring.
---
+++ 02_Array_Destructuring/swapping.js
@@ -0,0 +1,19 @@
+let animal1 = 'tiger'
+let animal2 = 'dog'
+
+
+console.log(animal1, animal2);
+
+
+
+// let temp = animal2;
+
+// animal2 = animal1;
+
+// animal1 = temp;
+
+
+[animal2,animal1] = [animal1,animal2]
+
+console.log(animal1, animal2);
+

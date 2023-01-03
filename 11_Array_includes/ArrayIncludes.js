54186d154e582b389f776ecfb9ca8812101d86b0
Prasenjit Das <prasanjitacademic@gmail.com>
2023-01-03 07:01:58 +0530

    Includes Method

---
+++ 11_Array_includes/ArrayIncludes.js
@@ -0,0 +1,32 @@
+//Array.includes()
+//Checks if the element is in the array
+//Useful in conditional statements
+
+const nums = [4,52,97,86,59];
+
+const friends = ['raju','sham','baburao'];
+
+// console.log(nums.includes(52));
+
+// const person1 = 'vikas'
+
+const person2 = 'raju'
+
+// const result1 = friends.includes(person1);
+
+// console.log(result1);
+
+// const result2 = friends.includes(person2,1);
+
+// console.log(result2);
+
+
+if (friends.includes(person2)) {
+    console.log('admi milgya');
+}
+
+
+
+
+
+

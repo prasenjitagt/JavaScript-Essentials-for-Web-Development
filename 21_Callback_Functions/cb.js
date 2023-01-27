fca0c641f2978eab1e3c9334f65015e056a890ff
Prasenjit Das <prasanjitacademic@gmail.com>
2023-01-27 10:09:20 +0530

    Callback Functions

---
+++ 21_Callback_Functions/cb.js
@@ -0,0 +1,50 @@
+//Callback Functions 
+//Used in array methods, setTimeout, event listeners etc
+
+// const Ucase = (val) =>{
+//     res = val.toUpperCase();
+//     console.log(res);
+// }
+
+
+// const channel = (val,cb) => {
+//     fullName = `${val} tripura classes`;
+//     cb(fullName);
+// }
+
+
+// channel('the',Ucase);
+
+
+
+const btn = document.getElementById("btn");
+
+btn.addEventListener('click',function kuch (){
+    console.log("me purana function hu");
+});
+
+
+// setTimeout(kuch,2000);
+
+
+arr = [1,5,9,8,4];
+
+// arr.map();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

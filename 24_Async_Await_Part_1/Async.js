8b2d57a5b7657c5fc660f2ef85fb24ed9893ea08
Prasenjit Das <businesspaymentsagt@gmail.com>
2023-01-30 15:47:54 +0530

    Async Await Part 1

---
+++ 24_Async_Await_Part_1/Async.js
@@ -0,0 +1,56 @@
+//Async Await
+
+//Async functions always return promise
+
+//Await waits untill the promise is settled
+
+//Error handling with -> try catch block
+
+//easy to read then promises
+
+
+// async function kuch () {
+//     return 'the tripura classes'
+// }
+
+// dusra = async () =>{
+//     const res = await kuch();
+//     console.log(res);
+// }
+
+// dusra();
+
+
+// console.log(kuch());
+
+
+const last = async () =>{
+    console.log('result k bad ka string');
+    res = await teesra();
+    console.log(res);
+}
+
+last();
+
+
+
+function teesra (){
+    return new Promise((resolve, reject) => {
+        // resolve('ho gya hai resolve')
+    })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
